cb5c3669acea8a6fdfc11b139c41eb036b06ea64
Adam Craven <git@adamcraven.me>
2015-07-20 19:31:57 +0100

    minified version added to src

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,18 @@
 var karma = require('karma').server;
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
   karma.start({
     configFile: __dirname + '/karma.conf.js',
     action: 'watch'
   });
+});
+
+gulp.task('build', function() {
+  return gulp.src('./angular-fng.js')
+    .pipe(rename('./angular-fng.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('.'))
 });
